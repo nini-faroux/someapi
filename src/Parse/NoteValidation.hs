4910babd819a4adb7130f4075fc54ef9643f503b
nini-faroux <nfarrel1@tcd.ie>
2021-10-21 17:58:43 +0100

    add makeDayInput function

--- src/Parse/NoteValidation.hs
+++ src/Parse/NoteValidation.hs
@@ -3,14 +3,14 @@ module Parse.NoteValidation
   ) where
 
 import RIO
-import RIO.Time (UTCTime, getCurrentTime, toGregorian, utctDay)
+import RIO.Time (UTCTime, getCurrentTime)
 import Servant (errBody, err400)
 import qualified Data.ByteString.Lazy.UTF8 as LB
 import Data.Validation (Validation(..))
 import Web.Model (NoteInput(..), Note(..))
 import Parse.Validation (VError(..))
 import App (App)
-import Parse.NoteTypes (DayInput(..), makeBody, makeTitle, validDayText)
+import Parse.NoteTypes (DayInput(..), makeDayInput, makeBody, makeTitle, validDayText)
 import qualified Parse.UserTypes as UserTypes
 
 -- | Parses the note into a valid form
@@ -18,9 +18,8 @@ import qualified Parse.UserTypes as UserTypes
 parseNote :: NoteInput -> App Note
 parseNote noteInput = do
   time' <- liftIO getCurrentTime
-  let (year, month, day') = toGregorian $ utctDay time'
-  let day = DayInput year month day'
-  case validNote noteInput time' day of
+  dayInput <- makeDayInput
+  case validNote noteInput time' dayInput of
     Failure errs -> throwIO err400 { errBody = errorsToBS errs }
     Success note -> return note
   where
